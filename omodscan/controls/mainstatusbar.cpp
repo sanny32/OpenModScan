ce799e727fa834336406b2ec22391c0c0d005080
Ананьев Александр <ananev-aa@nefteavtomatika.ru>
2023-01-12 15:31:57 +0300

    Correct style of statusbar

--- omodscan/controls/mainstatusbar.cpp
+++ omodscan/controls/mainstatusbar.cpp
@@ -14,16 +14,18 @@ MainStatusBar::MainStatusBar(QMdiArea* parent)
 
     _labelPolls = new QLabel(this);
     _labelPolls->setText(QString(tr("Polls: %1")).arg(0));
-    _labelPolls->setFrameShape(QFrame::StyledPanel);
+    _labelPolls->setFrameShadow(QFrame::Sunken);
+    _labelPolls->setFrameShape(QFrame::Panel);
     _labelPolls->setMinimumWidth(120);
 
     _labelResps = new QLabel(this);
     _labelResps->setText(QString(tr("Resps: %1")).arg(0));
-    _labelResps->setFrameShape(QFrame::StyledPanel);
+    _labelResps->setFrameShadow(QFrame::Sunken);
+    _labelResps->setFrameShape(QFrame::Panel);
     _labelResps->setMinimumWidth(120);
 
-    addPermanentWidget(_labelResps);
     addPermanentWidget(_labelPolls);
+    addPermanentWidget(_labelResps);
 }
 
 ///

f929475a4851b52d8bf82eba502bfd44f8721e39
Ананьев Александр <ananev-aa@nefteavtomatika.ru>
2023-02-03 08:41:25 +0300

    Correct for translating in future

--- omodscan/controls/statisticwidget.cpp
+++ omodscan/controls/statisticwidget.cpp
@@ -71,6 +71,6 @@ void StatisticWidget::on_pushButtonResetCtrs_clicked()
 ///
 void StatisticWidget::updateStatistic()
 {
-    ui->labelNumberOfPolls->setText(QString("Number of Polls: %1").arg(_numberOfPolls));
-    ui->labelValidSlaveResponses->setText(QString("Valid Slave Responses: %1").arg(_validSlaveResponses));
+    ui->labelNumberOfPolls->setText(QString(tr("Number of Polls: %1")).arg(_numberOfPolls));
+    ui->labelValidSlaveResponses->setText(QString(tr("Valid Slave Responses: %1")).arg(_validSlaveResponses));
 }

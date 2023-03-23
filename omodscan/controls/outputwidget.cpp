cc3c1c64e2c925db46bbe0a14c2fc6f6baf61d23
Ананьев Александр <ananev-aa@nefteavtomatika.ru>
2023-03-23 16:06:17 +0300

    Correct OutputListModel on data update

--- omodscan/controls/outputwidget.cpp
+++ omodscan/controls/outputwidget.cpp
@@ -371,8 +371,6 @@ void OutputListModel::update()
 ///
 void OutputListModel::updateData(const QModbusDataUnit& data)
 {
-    beginResetModel();
-
     _lastData = data;
 
     const auto mode = _parentWidget->dataDisplayMode();
@@ -426,7 +424,7 @@ void OutputListModel::updateData(const QModbusDataUnit& data)
         }
     }
 
-    endResetModel();
+    emit dataChanged(index(0), index(rowCount() - 1), QList<int>() << Qt::DisplayRole);
 }
 
 ///

80c78cea657fd05603258d9e2e917a964c3ae8f5
sanny32 <mail@ananev.org>
2024-12-29 20:42:06 +0300

    Developing ascii view

--- omodscan/formatutils.h
+++ omodscan/formatutils.h
@@ -240,6 +240,18 @@ inline QString formatAsciiValue(QModbusDataUnit::RegisterType pointType, quint16
         case QModbusDataUnit::HoldingRegisters:
         case QModbusDataUnit::InputRegisters:
         {
+            static const QMap<quint8, QString> __non_printable_ascii = {
+                {0x00, "NULL"}, {0x01, "SOH"}, {0x02, "STX"}, {0x03, "ETX"},
+                {0x04, "EOT"},  {0x05, "ENQ"}, {0x06, "ACK"}, {0x07, "BEL"},
+                {0x08, "BS"},   {0x09, "TAB"}, {0x0A, "LF"},  {0x0B, "VT"},
+                {0x0C, "FF"},   {0x0D, "CR"},  {0x0E, "SO"},  {0x0F, "SI"},
+                {0x10, "DLE"},  {0x11, "DC1"}, {0x12, "DC2"}, {0x13, "DC3"},
+                {0x14, "DC4"},  {0x15, "NAK"}, {0x16, "SYN"}, {0x17, "ETB"},
+                {0x18, "CAN"},  {0x19, "EM"},  {0x1A, "SUB"}, {0x1B, "ESC"},
+                {0x1C, "FS"},   {0x1D, "GS"},  {0x1E, "RS"},  {0x1F, "US"},
+                {0x7F, "DEL"}
+            };
+
             quint8 lo, hi;
             breakUInt16(value, lo, hi, ByteOrder::LittleEndian);
 
@@ -250,8 +262,10 @@ inline QString formatAsciiValue(QModbusDataUnit::RegisterType pointType, quint16
             for(auto&& cb : bytes)
             {
                 const QChar c(cb);
-                if(c.isPrint() && cb != 13)
+                if(c.isPrint() && !c.isSpace())
                     chars.append(c);
+                /*else if(__non_printable_ascii.contains(cb))
+                    chars.append(QString("[%1]").arg(__non_printable_ascii[cb]));*/
                 else
                     chars.append(QString("\\x%1").arg(QString::number(cb, 16).toUpper(), 2, '0'));
             }

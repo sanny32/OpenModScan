288a55c72b9aa0116334e0fa2123120ab1b75922
sanny32 <mail@ananev.org>
2025-09-04 17:15:34 +0300

    Correcting build script

--- build.sh
+++ build.sh
@@ -438,7 +438,7 @@ echo "Build finished successfully in $BUILD_DIR."
 echo ""
 
 NINJA_VER=$(ninja --version)
-if verlt "$NINJA_VER" "1.11"; then
+if verlt "$NINJA_VER" "1.11.0"; then
     echo "To install Open ModScan, run:"
     echo ""
     if [ "$EUID" -eq 0 ]; then

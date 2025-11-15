348dbfeacf169da0549108886bcd14ab14cecb69
ananev <mail@ananev.org>
2025-11-15 11:55:41 +0300

    Force the light theme at linux (#57)

--- omodscan/main.cpp
+++ omodscan/main.cpp
@@ -2,6 +2,22 @@
 #include "mainwindow.h"
 
 ///
+/// \brief The PaletteGuard class
+///
+class PaletteGuard : public QObject {
+public:
+    PaletteGuard(QObject* parent) : QObject(parent) { }
+    bool eventFilter(QObject *obj, QEvent *ev) override {
+        if (ev->type() == QEvent::ApplicationPaletteChange) {
+            QTimer::singleShot(0, [](){
+                QApplication::setPalette(QApplication::style()->standardPalette());
+            });
+        }
+        return QObject::eventFilter(obj, ev);
+    }
+};
+
+///
 /// \brief main
 /// \param argc
 /// \param argv
@@ -12,7 +28,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName(APP_NAME);
     a.setApplicationVersion(APP_VERSION);
+
+#ifdef Q_OS_WIN
     a.setStyle("windowsvista");
+#else
+    a.setStyle("Fusion");
+    qApp->installEventFilter(new PaletteGuard(qApp));
+#endif
 
     MainWindow w;
     w.show();

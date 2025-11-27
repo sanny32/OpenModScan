13b98f0ed4bccf95e7e8d32182ced9b17c2b08e1
sanny32 <mail@ananev.org>
2025-11-27 09:52:37 +0300

    Added saving/restoring window pos in xml serialization

--- omodscan/formmodsca.h
+++ omodscan/formmodsca.h
@@ -414,6 +414,10 @@ inline QXmlStreamWriter& operator <<(QXmlStreamWriter& xml, FormModSca* frm)
     xml.writeAttribute("Maximized", boolToString(wnd->isMaximized()));
     xml.writeAttribute("Minimized", boolToString(wnd->isMinimized()));
 
+    const auto windowPos = wnd->pos();
+    xml.writeAttribute("Left", QString::number(windowPos.x()));
+    xml.writeAttribute("Top", QString::number(windowPos.y()));
+
     const auto windowSize = (wnd->isMinimized() || wnd->isMaximized()) ? wnd->sizeHint() : wnd->size();
     xml.writeAttribute("Width", QString::number(windowSize.width()));
     xml.writeAttribute("Height", QString::number(windowSize.height()));
@@ -523,6 +527,15 @@ inline QXmlStreamReader& operator >>(QXmlStreamReader& xml, FormModSca* frm)
 
                 const auto wnd = frm->parentWidget();
                 if (wnd) {
+                    if(windowAttrs.hasAttribute("Left") && windowAttrs.hasAttribute("Top")) {
+                        bool okLeft, okTop;
+                        const int left = windowAttrs.value("Left").toInt(&okLeft);
+                        const int top = windowAttrs.value("Top").toInt(&okTop);
+                        if(okLeft && okTop) {
+                            wnd->move(left, top);
+                        }
+                    }
+
                     if (windowAttrs.hasAttribute("Width") && windowAttrs.hasAttribute("Height")) {
                         bool okWidth, okHeight;
                         const int width = windowAttrs.value("Width").toInt(&okWidth);

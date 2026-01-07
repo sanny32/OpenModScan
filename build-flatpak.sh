4250619c4ced52e741626fc17b6cb89f5e7cbadc
sanny32 <mail@ananev.org>
2026-01-07 20:23:25 +0300

    Corrected build flatpak script

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-verlte() {
-    # $1 <= $2 ?
+verlt() {
+    # $1 < $2 ?
+    [ "$1" != "$2" ] && \
     [ "$1" = "$(printf '%s\n%s' "$1" "$2" | sort -V | head -n1)" ]
 }
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 # Check if flatpak is installed
 if ! command -v flatpak &> /dev/null; then
     echo -e "\033[31mError: flatpak is not installed. Please install it and try again.\033[0m"
     exit 1
 fi
+
+# Add flathub remote if not exists
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Check if flatpak-builder is installed
 if ! command -v flatpak-builder &> /dev/null; then
@@ -19,7 +25,7 @@ fi
 
 FB_APP="flatpak-builder"
 FB_VERSION=$($FB_APP --version 2>/dev/null | grep -o '[0-9]\+\.[0-9]\+' | head -1)
-if verlte "$FB_VERSION" "1.4"; then
+if verlt "$FB_VERSION" "1.4"; then
     echo -e "\033[33mWarning: flatpak-builder version must be 1.4 or higher.\033[0m"
 
     if flatpak info --user org.flatpak.Builder > /dev/null 2>&1; then
@@ -31,9 +37,6 @@ if verlte "$FB_VERSION" "1.4"; then
 fi
 
 cd .github/flatpak || exit 1
-
-# Add flathub remote if not exists
-flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install required runtimes and tools
 if flatpak info --user org.kde.Sdk//6.9 > /dev/null 2>&1; then
@@ -53,6 +56,9 @@ echo -e "Done\n"
 echo -e "\033[32mCreating flatpak bundle...\033[0m"
 flatpak build-bundle repo io.github.sanny32.omodscan.flatpak io.github.sanny32.omodscan stable
 echo -e "Done\n"
+
+# Move bundle to script directory
+mv io.github.sanny32.omodscan.flatpak "$SCRIPT_DIR"/ || exit 1
 
 # Cleanup
 echo -e "\033[32mCleanup...\033[0m"

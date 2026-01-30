6253f9b03b263264098d29f604f4a07b4dba0fcd
sanny32 <mail@ananev.org>
2026-01-30 08:47:11 +0300

    Update metainfo, build flatpak script and workflows

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -36,8 +36,6 @@ if verlt "$FB_VERSION" "1.4"; then
     FB_APP="flatpak run org.flatpak.Builder"
 fi
 
-cd .github/linux/flatpak || exit 1
-
 # Install required runtimes and tools
 if flatpak info --user org.kde.Sdk//6.9 > /dev/null 2>&1; then
     flatpak --user update -y org.kde.Sdk//6.9 || exit 1
@@ -54,9 +52,21 @@ if [ -z "${REF_NAME:-}" ]; then
        echo -e "\033[33mWarning: failed to detect Git branch name. Using 'main' as fallback.\033[0m"
        REF_NAME="main"
     else
-       echo "Detected Git branch: $REF_NAME"
+       echo -e "Detected Git branch: $REF_NAME\n"
     fi   
 fi
+
+# Extract version
+FULL_VERSION=$(grep -oP 'VERSION\s+\K[0-9]+\.[0-9]+\.[0-9]+' src/CMakeLists.txt)
+if [ "$REF_NAME" = "dev" ]; then
+   MAJOR_MINOR=$(echo "$FULL_VERSION" | cut -d. -f1,2)
+   APP_VERSION="${MAJOR_MINOR}~dev"
+else
+  APP_VERSION="$FULL_VERSION"
+fi
+echo -e "Extracted version: $APP_VERSION\n"
+
+cd .github/linux/flatpak || exit 1
 
 # Create desktop file
 sed -e "s|@APP_EXEC@|/app/bin/omodscan|g" \
@@ -66,6 +76,8 @@ sed -e "s|@APP_EXEC@|/app/bin/omodscan|g" \
 # Create metainfo file
 sed -e "s|@REF_NAME@|$REF_NAME|g" \
     -e "s|@COMPONENT_ID@|io.github.sanny32.omodscan|g" \
+    -e "s|@APP_VERSION@|$APP_VERSION|g" \
+    -e "s|@BUILD_DATE@|$(date +%Y-%m-%d)|g" \
     ../usr/share/metainfo/omodscan.metainfo.xml.in > ../usr/share/metainfo/omodscan.metainfo.xml
 
 # Build project

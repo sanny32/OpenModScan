2abbcbf4b3f81620f5c38c5c523db9e3f0c6ad41
sanny32 <mail@ananev.org>
2026-02-12 12:32:51 +0300

    Deprecated binary saving in the application

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -438,8 +438,7 @@ void MainWindow::on_actionSave_triggered()
     if(filename.isEmpty())
         ui->actionSaveAs->trigger();
     else {
-        const auto format = filename.endsWith(".xml", Qt::CaseInsensitive) ? SerializationFormat::Xml : SerializationFormat::Binary;
-        saveMdiChild(frm, format);
+        saveMdiChild(frm, SerializationFormat::Xml);
     }
 }
 
@@ -451,7 +450,7 @@ void MainWindow::on_actionSaveAs_triggered()
     auto frm = currentMdiChild();
     if(!frm) return;
 
-    saveAs(frm, static_cast<SerializationFormat>(-1));
+    saveAs(frm, SerializationFormat::Xml);
 }
 
 ///

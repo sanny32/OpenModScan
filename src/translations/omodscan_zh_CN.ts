87fae0e58152e2fa29cd57fa2e3aa48774567e09
sanny32 <mail@ananev.org>
2026-03-13 11:04:13 +0300

    Update UI properties for dialog components and translations

--- src/translations/omodscan_zh_CN.ts
+++ src/translations/omodscan_zh_CN.ts
@@ -2053,7 +2053,7 @@ Valid Slave Responses: %2</source>
 <context>
     <name>HexViewButton</name>
     <message>
-        <location filename="../controls/hexviewbutton.cpp" line="29"/>
+        <location filename="../controls/hexviewbutton.cpp" line="32"/>
         <source>Hex View</source>
         <translation type="unfinished">十六进制视图</translation>
     </message>
--- src/translations/omodscan_zh_TW.ts
+++ src/translations/omodscan_zh_TW.ts
@@ -2052,7 +2052,7 @@ Valid Slave Responses: %2</source>
 <context>
     <name>HexViewButton</name>
     <message>
-        <location filename="../controls/hexviewbutton.cpp" line="29"/>
+        <location filename="../controls/hexviewbutton.cpp" line="32"/>
         <source>Hex View</source>
         <translation type="unfinished">十六進制視圖</translation>
     </message>
